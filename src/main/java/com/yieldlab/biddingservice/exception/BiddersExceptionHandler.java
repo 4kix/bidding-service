16757dc0375377460b489fa45300f6ff2c12f597
4kix <falko.ilya@gmail.com>
2023-02-05 23:36:54 +0300

    change response code for no bids

--- src/main/java/com/yieldlab/biddingservice/exception/BiddersExceptionHandler.java
+++ src/main/java/com/yieldlab/biddingservice/exception/BiddersExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.yieldlab.biddingservice.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class BiddersExceptionHandler {
 
     @ExceptionHandler(NoBidsForAuctionException.class)
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public void handleNoBidsForAuctionException() {
     }
 
